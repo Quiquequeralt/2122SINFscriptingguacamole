adf23a98b653cb92c33bbada26cc49440df92140
Aaron/Joker <arosa09032002@gmail.com>
2022-04-08 11:45:30 +0200

    Is madoify the file

--- Projecte Script-GitHub/testDir.sh
+++ Projecte Script-GitHub/testDir.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-dir11 = testDir
+dir11=testDir
     if [ -d "$dir11" ]
         then
             echo "Directory has found"
         else
-            echi "Directory has not been found"
+            echo "Directory has not been found"
     fi
 
 exit 0
