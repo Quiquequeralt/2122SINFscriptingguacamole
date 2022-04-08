9a33d6fe029b21268ab2b69b2ce89d44bcce7627
Aaron/Joker <arosa09032002@gmail.com>
2022-04-08 11:20:18 +0200

    Is update

--- Projecte Script-GitHub/Yes[Expresion].sh
+++ Projecte Script-GitHub/Yes[Expresion].sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 filename=myfile.txt
-if [ -f "$filename" ];
-then
-    echo "$filename has found."
-else
-    echo "filename has not been found"
-fi
+    if [ -f "$filename" ];
+        then
+            echo "$filename has found."
+        else
+            echo "filename has not been found"
+    fi
+
+exit 0
