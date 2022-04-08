e2e342de1b6de2d8c4f232e6b223c558bb46d559
Aaron/Joker <arosa09032002@gmail.com>
2022-04-08 10:08:14 +0200

    There are a three forms comprove if exists a file, a form is this

---
+++ Projecte Script-GitHub/Prove[Expresion].sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+filename = file1
+    if test -f "$filename";
+        then
+            echo "$file has found."
+        else
+            echo "$file has not been found"
+        fi
+
+exit 0
