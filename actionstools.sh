e7c8c5a130cbc996cae4b1a88d1d8959b2c271df
Quique <enrsan.alu@iesbenigaslo.es>
2022-04-13 10:09:33 +0200

    Final

--- actionstools.sh
+++ actionstools.sh
@@ -1,3 +1,4 @@
+ 
 #!/bin/bash
 # Simple script to launch several tools from the command line
 
@@ -6,21 +7,67 @@ source toolsmenu.sh
 # ...
 
 # Main body
-Action=$(simple_menu)
-case $Action
+read $Action
+case $Action in
   1 )
     #launch antivirus tools
     # ...
+    LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+    DIRTOSCAN=".";
+
+    for S in ${DIRTOSCAN}; do
+    DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
+
+    echo "Starting a daily scan of "$S" directory.
+    Amount of data to be scanned is "$DIRSIZE".";
+
+    clamscan -ri "$S" >> "$LOGFILE";
+
+    # get the value of "Infected lines"
+    MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
+
+
+    if [ "$MALWARE" -ne "0" ];then
+    echo "Malware Found";
+    fi
+    done
+
+    exit 0
     ;;
 
   2 )
     #Analyse a directory
     #Check permissions
     # ...
+    echo "Enter your filename"
+    read newfile1
+    if [ -f "$newfile1" ]
+    then
+            echo "File is found"
+    else
+        echo "File is not found"
+    fi
+
+
+    exit 0
     ;;
   3 )
     #Make a back up with tar
     # ...
+
+        echo 'What do you want to copy'
+        read backup_files
+
+        echo 'Where'
+        read dest
+
+        day=$(date +%A)
+        hostname=$(hostname -s)
+        archive_file="$hostname-$day.tgz"
+
+        tar czf $dest/$archive_file $backup_files
+
+        echo "Backup finished"
     ;;
 
   * )
--- toolsmenu.sh
+++ toolsmenu.sh
@@ -1,13 +1,13 @@
+ 
 
 # File that contains functions for menues and browsing
 
 main_menu () {
-  echo "Please, choose an option:"
-  echo "1 .- Virus scan options"
-  echo "2 .- Analyse directories"
-  echo "3 .- Whatever..."
-  read $Option
-exit $Option
+  echo "Please, choose an option:">&2
+  echo "1 .- Virus scan options">&2
+  echo "2 .- Analyse directories">&2
+  echo "3 .- Whatever...">&2
+
 }
 
 provide_dir () {
